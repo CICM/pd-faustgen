333fcfd3363ad8f494a779ed6327c17c0f35981f
Pierre <guillotpierre6@gmail.com>
2018-05-03 15:05:58 +0200

    fixed extern declaration for Windows

--- src/faust_tilde.c
+++ src/faust_tilde.c
@@ -679,7 +679,7 @@ static void *faust_tilde_new(t_symbol* s, int argc, t_atom* argv)
     return x;
 }
 
-void faust_tilde_setup(void)
+EXTERN void faust_tilde_setup(void)
 {
     t_class* c = class_new(gensym("faust~"),
                            (t_newmethod)faust_tilde_new, (t_method)faust_tilde_free,
