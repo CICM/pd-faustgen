5b28f5b592d62fdf4034245c1a4871b090565218
pierreguillot <guillotpierre6@gmail.com>
2018-05-30 13:33:01 +0200

    test 1

--- FaustLib.cmake
+++ FaustLib.cmake
@@ -41,7 +41,7 @@ set(WASM_BACKEND   OFF                            CACHE STRING  "Include WASM ba
 add_subdirectory(./faust/build EXCLUDE_FROM_ALL)
 
 if(MSVC)
-    set_property(TARGET staticlib APPEND_STRING PROPERTY COMPILE_FLAGS " /EHsc ")
+    #set_property(TARGET staticlib APPEND_STRING PROPERTY COMPILE_FLAGS " /EHsc ")
     set_property(TARGET staticlib APPEND_STRING PROPERTY COMPILE_FLAGS " /D WIN32 ")
 endif()
 
