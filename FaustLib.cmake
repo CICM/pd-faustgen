41e34de2f4d8d96ab911d200fe329e6defcf715f
pierreguillot <guillotpierre6@gmail.com>
2018-05-30 13:04:36 +0200

    clean FaustLin

--- FaustLib.cmake
+++ FaustLib.cmake
@@ -1,16 +1,6 @@
 ## Create Faust Lib
 message(STATUS "Faust Library")
 
-## For Windows use static runtime instead of default dynamic runtime
-if(MSVC)
-    set(CompilerFlags
-        CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE)
-    foreach(CompilerFlag ${CompilerFlags})
-      string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
-    endforeach()
-endif()
-
 ## Save the llvm directory and change it for subdirectory
 if(DEFINED LLVM_DIR)
   set(LLVM_DIR_TEMP   ${LLVM_DIR})
@@ -43,27 +33,6 @@ add_subdirectory(./faust/build EXCLUDE_FROM_ALL)
 if(MSVC)
     set_property(TARGET staticlib APPEND_STRING PROPERTY COMPILE_FLAGS " /EHsc ")
     set_property(TARGET staticlib APPEND_STRING PROPERTY COMPILE_FLAGS " /D WIN32 ")
-
-    set(TargetFlags COMPILE_FLAGS COMPILE_DEFINITIONS COMPILE_OPTIONS
-    INTERFACE_COMPILE_DEFINITIONS
-    INTERFACE_COMPILE_FEATURES
-    INTERFACE_COMPILE_OPTIONS
-    STATIC_LIBRARY_FLAGS_RELEASE
-    STATIC_LIBRARY_FLAGS_DEBUG
-    STATIC_LIBRARY_FLAGS
-    COMPILE_DEFINITIONS_RELEASE
-    COMPILE_DEFINITIONS_DEBUG
-)
-    foreach(TargetFlag ${TargetFlags})
-        get_target_property(STATIC_LIB_CURRENT_FLAGS staticlib ${TargetFlag})
-        message(STATUS "${TargetFlag}: ${STATIC_LIB_CURRENT_FLAGS}")
-
-        #if(NOT ${STATIC_LIB_CURRENT_FLAGS} STREQUAL "STATIC_LIB_CURRENT_FLAGS-NOTFOUND")
-
-        string(REPLACE "/MD" "/MT" ${STATIC_LIB_CURRENT_FLAGS} "${${STATIC_LIB_CURRENT_FLAGS}}")
-        set_target_properties(staticlib PROPERTIES ${TargetFlag} ${STATIC_LIB_CURRENT_FLAGS})
-        message(STATUS "${TargetFlag}: ${STATIC_LIB_CURRENT_FLAGS}")
-    endforeach()
 endif()
 
 ## Restore llvm directory
